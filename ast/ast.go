73bb5a134fbf0980cb5a03ae4ef122c9012a26df
dchnsk <leriidchnsk@gmail.com>
2023-08-06 00:31:30 +0200

    feat: add basic AST interfaces and structs

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/vdchnsk/i-go/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) expressionNode()      {}
+
+type LetStatement struct {
+	Token      token.Token
+	Identifier *Identifier
+	Value      Expression
+}
+
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) statementNode()       {}
