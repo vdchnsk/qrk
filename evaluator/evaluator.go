8cb2f40ac5c150baf6312925ff79e410994d6b85
dchnsk <leriidchnsk@gmail.com>
2023-10-15 00:32:57 +0200

    feat: add > < != == infix operators evaluation mechanism

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -86,6 +86,14 @@ func evalInfixIntExpression(operator string, left, right object.Object) object.O
 		return &object.Integer{Value: leftVal / rightVal}
 	case token.ASTERISK:
 		return &object.Integer{Value: leftVal * rightVal}
+	case token.LT:
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case token.GT:
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case token.EQ:
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case token.NOT_EQ:
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -57,6 +57,12 @@ func TestEvalBooelanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 < 1", false},
+		{"1 > 2", false},
+		{"1 != 2", true},
+		{"1 == 2", false},
+		{"1 == 1", true},
 	}
 
 	for _, tt := range tests {
