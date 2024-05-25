7f7dfbf4a291d8121f2b89d85418d5a76eb529d2
Valerii Dychanskyi <dichansky@gmail.com>
2024-05-25 13:02:43 +0200

    refactor: extract repl constants into a separate file, early return from repl loop if there is nothing to evaluate

---
+++ cmd/repl/constants.go
@@ -0,0 +1,6 @@
+package repl
+
+const (
+	REPL_WELCOME_MESSAGE = "Welcome to qrk, the language that's easier to learn than to pronounce. (Seriously, how do you say 'qrk'?)"
+	REPL_PROMPT_MESSAGE  = "> "
+)
--- cmd/repl/repl.go
+++ cmd/repl/repl.go
@@ -11,16 +11,13 @@ import (
 	"github.com/vdchnsk/qrk/src/parser"
 )
 
-const PRMOPT = "> "
-const REPL_WELCOME_MESSAGE = "Welcome to qrk, the language that's easier to learn than to pronounce. (Seriously, how do you say 'qrk'?)"
-
 func Start(in io.Reader, out io.Writer) {
 	fmt.Println(REPL_WELCOME_MESSAGE)
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
 
 	for {
-		fmt.Print(PRMOPT)
+		fmt.Print(REPL_PROMPT_MESSAGE)
 		scanned := scanner.Scan()
 
 		if !scanned {
@@ -38,15 +35,19 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+
+		if evaluated == nil {
+			continue
 		}
+
+		io.WriteString(out, evaluated.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, "Syntax error! \n")
+
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
