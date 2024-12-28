d381945a5abefffb5bc752a94a9c897ebf39bba2
Valerii Dychanskyi <dichansky@gmail.com>
2024-12-28 20:19:09 +0100

    refactor: return s.Value directly from String Object Inspect()

--- src/object/object.go
+++ src/object/object.go
@@ -89,7 +89,7 @@ type String struct {
 }
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
-func (s *String) Inspect() string  { return fmt.Sprintf(s.Value) }
+func (s *String) Inspect() string  { return s.Value }
 func (s *String) HashKey() HashKey {
 	hash := fnv.New64a()
 	hash.Write([]byte(s.Value))
